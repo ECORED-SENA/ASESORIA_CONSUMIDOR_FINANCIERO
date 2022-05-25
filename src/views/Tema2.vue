3837ed049dbc172fca1d7af581c5187bbfda4ad5
Luis Urueta <lgurueta@misena.edu.co>
2022-05-25 15:23:22 -0500

    V03

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -56,7 +56,7 @@
           figure
             img.br01(src='@/assets/curso/temas/t2_02.svg', alt='La asesoría')
       
-    .row.align-items-center
+    .row.align-items-center.mb-5
       .col-lg-4.mb-4.mb-lg-0
         figure
           img.br01(src='@/assets/curso/temas/t2_03.png', alt='Asesor comercial de productos financieros')
@@ -68,6 +68,82 @@
         p El servicio es definido por el autor como las actividades destinadas a satisfacer la necesidad del consumidor, todas aquellas estrategias mediante las cuales se interactúa con el cliente con la finalidad de que adquiera un producto o servicio y, de esta forma, dar solución a un problema o una necesidad.
 
         p Por lo general las entidades financieras ofrecen productos y servicios intangibles, debido a esto, el servicio debe ser una política de calidad dentro de estas entidades, brindando seguridad y confianza al consumidor, lo que minimiza los niveles de incertidumbre o temor al creer que algo no les pertenece debido a que no es tangible.
+    
+    #t2_1.titulo-segundo.color-acento-contenido
+      h2 2.1. Proceso de atención y servicio al cliente
+
+    .row.align-items-center.mb-5
+      .col-lg-7.mb-4.mb-lg-0
+        p Las entidades financieras deben tener una estructura clara y definida del proceso de atención y servicio al cliente dentro de su marco estratégico y de planeación de la entidad, en el cual se involucra, principalmente, el asesor comercial, al ser el primer contacto entre la entidad y el consumidor.
+
+        p Es por esto que un excelente servicio al cliente debe ser una de las principales estrategias de las organizaciones, de este depende la fidelidad del cliente. Las organizaciones pueden tener un excelente producto o portafolio de servicios, pero esto no influye si el servicio al cliente no es el adecuado, lo cual no genera ventas ni ingresos. 
+
+        p En las entidades financieras, el servicio al cliente es un factor relevante de generación de valor, por lo cual se debe realizar un adecuado diseño de construcción, estrategia y proceso de enfoque al cliente, como se detalla a continuación. 
+
+      .col-lg-5
+        figure
+          img.br01(src='@/assets/curso/temas/t2_04.png', alt='Proceso de atención y servicio al cliente')
+      
+    .video.mb-5
+      iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+    
+    .row.align-items-center.mb-5
+      .col-lg-5.mb-4.mb-lg-0
+        .titulo-sexto.color-acento-contenido
+          h5 Figura 2.
+          span ISO 9001:2015 - Cliente
+        figure
+          img.br01(src='@/assets/curso/temas/t2_05.svg', alt='Sector financiero')
+      .col-lg-7
+        p Como lo detalla la norma internacional ISO 9001:2015, Sistema de gestión de calidad, el enfoque al cliente es indispensable dentro de las organizaciones en su planeación estratégica, lo que les permite:
+
+        ul.lista-ul--color
+          li 
+            i.fas.fa-donate
+            | Ofrecer productos y servicios que satisfacen las necesidades de los consumidores.
+          li 
+            i.fas.fa-donate
+            | Gestionar los riesgos de la entidad.
+          li 
+            i.fas.fa-donate
+            | Lograr la conformidad en cada una de sus gestiones y ejes estratégicos.
+        
+        p En cuanto al enfoque de la alta dirección, se debe tener en cuenta el cumplimiento de los requisitos del cliente y la legalidad, así como también determinar y considerar los riesgos y oportunidades inherentes a los productos y servicios, y la capacidad de aumentar la satisfacción del cliente. Como se indica, estas estrategias deben ser lideradas por la alta dirección o la alta gerencia de la organización.
+
+        p En cuanto al proceso de operación, uno de los factores más relevantes es la comunicación, en la cual se debe tener en cuenta:
+
+        ul.lista-ul--color
+          li 
+            i.fas.fa-donate
+            | Información de los productos y servicios.
+          li 
+            i.fas.fa-donate
+            | Tratar las consultas, los contratos o los pedidos, incluyendo los cambios.
+          li 
+            i.fas.fa-donate
+            | Obtener la retroalimentación relativa a los productos y servicios, incluyendo las quejas de los clientes.
+          li 
+            i.fas.fa-donate
+            | Manipular o controlar la propiedad del cliente.
+          li 
+            i.fas.fa-donate
+            | Establecer los requisitos específicos para las acciones de contingencia, cuando sea pertinente.
+    
+    .row.align-items-center.mb-4
+      .col-lg-7.mb-4.mb-lg-0
+        p Dentro del proceso de servicio al cliente, se tiene acceso a la información del cliente, por lo cual, de acuerdo con la ISO 9001:2015, la organización debe cuidar la propiedad perteneciente a los clientes o a proveedores externos mientras esté bajo el control de la organización o esté siendo utilizada por la misma; esto como un proceso de operación.
+        .BG20.p-3.mb-4
+          p.mb-0 En cuanto a la evaluación de desempeño, se debe propender, como objetivo, a la satisfacción del cliente, por lo cual se debe realizar el correcto seguimiento de la percepción de los clientes, en el cual se evidencia el cumplimiento de sus necesidades y expectativas.
+        .row.align-items-center.mb-5
+          .col-lg-2.mb-4.mb-lg-0
+            figure
+              img.br01(src='@/assets/curso/temas/t2_07.jpg', alt='Monedas')
+          .col-lg-10
+            p Como norma internacional y siguiendo los lineamientos del sistema de gestión de calidad, se debe concebir este proceso como la línea base para todas las organizaciones en cuanto a su proceso de servicio al cliente, sin embargo, las características de cada una pueden demandar unos procesos ajustados de  acuerdo con las necesidades, sin olvidar que el fin último es la satisfacción y exclusividad del cliente.
+      .col-lg-5
+        figure
+          img.br01(src='@/assets/curso/temas/t2_06.jpg', alt='cuidar la propiedad perteneciente a los clientes')
+
 
 
 </template>
